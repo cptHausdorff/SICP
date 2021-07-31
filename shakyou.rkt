411fe049711118a1574ad7cd3f5011c5fe4144fa
cptHausdorff <87052721+cptHausdorff@users.noreply.github.com>
2021-07-31 14:01:11 +0900

    Update shakyou.rkt
    1章終わり

--- shakyou.rkt
+++ shakyou.rkt
@@ -48,15 +48,17 @@
   (/ (+ x y) 2))
 ;; sqrt
 (define (sqrt x)
-  (define (good-enough? guess)
-    (< (abs (- (square guess) x)) 0.001))
-  (define (improve guess)
-    (average guess (/ x guess)))
-  (define (sqrt-iter guess)
-    (if (good-enough? guess)
-        guess
-        (sqrt-iter (improve guess))))
-  (sqrt-iter 1.0))
+  "y -> x/y の不動点 y"
+  (fixed-point-of-transform (lambda (y) (/ x y))
+                            average-damp
+                            1.0))
+#|
+(define (sqrt x)
+  "y^2-x の根 y"
+  (fixed-point-of-transform (lambda (y) (- (square y) x))
+                            newton-transform
+                            1.0))
+|#
 ;; factorial
 (define (factorial n)
   (fact-iter 1 1 n))
@@ -149,14 +151,71 @@
           b)
      dx))
 
+(define (search f neg-point pos-point)
+  (let ((midpoint (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+        midpoint
+        (let ((test-value (f midpoint)))
+          (cond ((positive? test-value)
+                 (search f neg-point midpoint))
+                ((negative? test-value)
+                 (search f midpoint pos-point))
+                (else midpoint))))))
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.001))
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+        (b-value (f b)))
+    (cond ((and (negative? a-value) (positive? b-value))
+           (search f a b))
+          ((and (negative? b-value) (positive? a-value))
+           (search f b a))
+          (else
+           (error "Values are not of opposite sign" a b)))))
+;; fixed point
+(define (fixed-point f first-guess)
+  (let ((tolerance 0.00001))
+    (define (close-enough? v1 v2)
+      (< (abs (- v1 v2)) tolerance))
+    (define (try guess)
+      (let ((next (f guess)))
+        (if (close-enough? guess next)
+            next
+            (try next))))
+    (try first-guess)))
 
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+;; newton's method
+(define (deriv g)
+  (let ((dx 0.0001))
+    (lambda (x)
+      (/ (- (g (+ x dx)) (g x))
+         dx))))
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
 
+#|
+(define (fixed-point f first-guess)
+  (let ((tolerance 0.00001))
+    (define (close-enough? v1 v2)
+      (< (abs (- v1 v2)) tolerance))
+    (define (try guess)
+      (let ((next (f guess)))
+        (if (close-enough? guess next)
+            next
+            (try next))))
+    (try first-guess)))
 
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+|#
 
-
-
-
-
+(define (fixed-point-of-transform g transform guess)
+  (fixed-point (transform g) guess))
 
 
 
